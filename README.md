01687a2ee22f75c666aa7a5847325537842e293e
vikash kumar <krvikash35@gmail.com>
2017-09-12 04:37:56 -0700

    updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejs-pure-http-server
 Simple static file server using pure Node.JS API
+
+### To run, will run on 3000 port
+```
+node server.js
+```
--- server.js
+++ server.js
@@ -1,34 +1,49 @@
 const http  = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const httpServer = http.createServer(requestResponseHandler);
 httpServer.listen(3000, () => {
   console.log('Node.JS static file server is listening on port 3000')
 })
-var requestResponseHandler = function(req, res){
-  let file = req.url.substring(1, req.url.length);
-  let contentType = getContentType(file);
-  sendResponse(file, contentType, res);
+function requestResponseHandler(req, res){
+  console.log(`Request came: ${req.url}`);
+  if(req.url === '/'){
+    sendResponse('index.html', 'text/html', res)
+  }else{
+    sendResponse(req.url, getContentType(req.url), res);
+  }
 }
 
-function sendResponse(file, contentType, res){
+function sendResponse(url, contentType, res){
+  let file = path.join(__dirname, url);
   fs.readFile(file, (err, content) => {
     if(err){
       res.writeHead(404);
       res.write(`File '${file}' Not Found!`);
       res.end();
-      console.log("Error while process client request \n", err);
+      console.log("Response: 404 ${file}, err");
     }else{
       res.writeHead(200, {'Content-Type': contentType});
       res.write(content);
       res.end();
-      console.log("response successfully sent");
+      console.log(`Response: 200 ${file}`);
     }
   })
 }
 
 
-function getContentType(file){
-  // let fileExtension = 
-
+function getContentType(url){
+  switch (path.extname(url)) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    case '.json':
+      return 'application/json';
+    default:
+      return 'application/octate-stream';
+  }
 }
